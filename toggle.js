3e3fc682e7fcb545816a88f6724362c5870ccc18
yangwonhee <yangwonhee@kakao.com>
2022-01-31 01:43:56 +0900

    darkmode erro reset & switch position

--- toggle.js
+++ toggle.js
@@ -1,18 +1,17 @@
-// const toggleBG = document.querySelector('.switch');
+// const darkModeBg = document.getElementById('BackGroundColor');
+// const switchButton = document.querySelector(".myCheck");
 
-// //darkmode
-// function darkModeTrans(){
-//     const darkModeBG = document.body;
-//     darkModeBG.classList.toggle("dark-mode");
-//     toggleBtnColor();
-// }
-const darkModeBg = document.getElementById('BackGroundColor');
-const switchButton = document.querySelector(".myCheck");
+// function darkModeFunction() {
+//     if (switchButton.checked == true){
+//       darkModeBg.classList.add("dark-mode")
+//     } else {
+//       darkModeBg.classList.remove("dark-mode")
+//     }
+//   }
 
-function darkModeFunction() {
-    if (switchButton.checked == true){
-      darkModeBg.classList.add("dark-mode")
-    } else {
-      darkModeBg.classList.remove("dark-mode")
-    }
-  }
+
+  const checkbox = document.getElementById('checkbox');
+
+  checkbox.addEventListener('change', ()=>{
+    document.body.classList.toggle('dark');
+  })
